7ec21f29b14c289525f8dc155a0872bca928f993
Inaldo Anjos <inaldoanjosjr@gmail.com>
2022-10-04 22:15:42 -0300

    Média 2

---
+++ bee - 1006.php
@@ -0,0 +1,11 @@
+<?php
+
+    $A = fgets(STDIN);
+    $B = fgets(STDIN);
+    $C = fgets(STDIN);
+
+    $MEDIA = number_format((($A * 2) + ($B * 3) + ($C * 5)) / 10, 1, '.', '');
+
+    printf("MEDIA = $MEDIA\n");
+
+?>
